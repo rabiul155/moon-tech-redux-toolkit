1c8d03d3482657a669990a18038723730eff7711
rabiul-repliq <rabiul.islam@repliq.dev>
2023-12-05 12:34:04 +0600

    first commit

--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -1,22 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import ProductCard from "../../components/ProductCard";
-import { toggleBrand, toggleStock } from "../../redux/actions/filterActions";
-import loadProductData from "../../redux/thunk/products/fetchProducts";
+import { toggle, toggleBrand } from "../../features/filter/filterSlice";
+import { getProducts } from "../../features/products/productsSlice";
 
 const Home = () => {
-  const filters = useSelector((state) => state.filter.filters);
-  const products = useSelector((state) => state.product.products);
+  const filters = useSelector((state) => state.filter);
+  const { products, isLoading } = useSelector((state) => state.products);
   const { brands, stock } = filters;
   const dispatch = useDispatch();
 
+  const base = process.env.REACT_APP_MOON_TECH_BASE_URL;
+  console.log(base);
+
   useEffect(() => {
-    dispatch(loadProductData());
-  }, [dispatch]);
+    dispatch(getProducts());
+  }, []);
 
   const activeClass = "text-white bg-indigo-500 border-white";
 
   let content;
+
+  if (isLoading) {
+    content = <div className=" text-lg text-center m-10">Loading....</div>;
+  }
 
   if (products.length) {
     content = products.map((product) => (
@@ -42,10 +49,10 @@ const Home = () => {
   }
 
   return (
-    <div className='max-w-7xl gap-14 mx-auto my-10'>
-      <div className='mb-10 flex justify-end gap-5'>
+    <div className="max-w-7xl gap-14 mx-auto my-10">
+      <div className="mb-10 flex justify-end gap-5">
         <button
-          onClick={() => dispatch(toggleStock())}
+          onClick={() => dispatch(toggle())}
           className={`border px-3 py-2 rounded-full font-semibold ${
             stock ? activeClass : null
           } `}
@@ -69,7 +76,7 @@ const Home = () => {
           Intel
         </button>
       </div>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14'>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14">
         {content}
       </div>
     </div>
